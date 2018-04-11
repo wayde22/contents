0f494a037986de377ea0ec9130d36b4b21f410c8
Wade Stevens <wayde22@gmail.com>
2018-04-11 18:05:58 -0500

    First commit

---
+++ src/main/java/com/wadestevens/contents/ContentsApplication.java
@@ -0,0 +1,12 @@
+package com.wadestevens.contents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentsApplication.class, args);
+	}
+}
